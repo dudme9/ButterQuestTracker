40656340ff39cc51e6ee4a074851d539ba058d93
dudme9 <59275387+dudme9@users.noreply.github.com>
2019-12-28 04:09:26 +0900

    Create koKR config.lua

---
+++ Locale/koKR config.lua
@@ -0,0 +1,137 @@
+ButterQuestTrackerLocale.locale['koKR'] = {
+    -- Quest Tracker
+
+    ['QT_QUESTS'] = "퀘스트",
+    ['QT_READY_TO_TURN_IN'] = "완료",
+    ['QT_FAILED'] = "퀘스트 실패, 포기하고 다시 시작",
+    ['QT_UNTRACK_QUEST'] = "퀘스트 추저 해제",
+    ['QT_VIEW_QUEST'] = "퀘스트 보기",
+    ['QT_WOWHEAD_URL'] = "|cff33ff99Wowhead|r URL",
+    ['QT_SHARE_QUEST'] = "퀘스트 공유",
+    ['QT_CANCEL_QUEST'] = "퀘스트 취소",
+    ['QT_ABANDON_QUEST'] = "퀘스트 포기",
+
+    -- Settings
+
+    ['SETTINGS_NAME'] = "Butter Quest Tracker |cFFFFFFFFv%s|r",
+    ['SETTINGS_ENABLED_NAME'] = "사용",
+    ['SETTINGS_FORMAT_NAME'] = "형식",
+
+    ['SETTINGS_DISPLAY_DUMMY_DATA_NAME'] = "가상 데이터 표시",
+    ['SETTINGS_DISPLAY_DUMMY_DATA_DESC'] = "추적기에 가상 데이터를 표시하면 시각효과를 보다 쉽게 조정할 수 있습니다.\n\n이것은 또한 현재 지역이 썬더블러프인 것처럼 작동합니다.",
+
+    -- Settings > Filters & Sorting
+
+    ['SETTINGS_FILTERS_AND_SORTING_TAB'] = "필터 및 정렬",
+
+    ['SETTINGS_AUTO_TRACK_UPDATED_QUESTS_NAME'] = "새로운 퀘스트를 자동으로 추적",
+    ['SETTINGS_AUTO_TRACK_UPDATED_QUESTS_DESC'] = "퀘스트가 수락 / 업데이트 되면 필터를 무시하고 자동으로 추적됩니다.\n\n|c00FF9696이 기능은 자동 추적을 사용하여 필터를 무시합니다. 따라서 이 옵션을 비활성화 하면 자동 추적도 지워집니다.|r",
+
+    ['SETTINGS_SORTING_NAME'] = "정렬",
+    ['SETTINGS_SORTING_DESC'] = "트래커에서 퀘스트가 어떻게 정렬되는지 지정하시오.",
+
+    ['SETTINGS_AUTO_HIDE_QUEST_HELPER_ICONS_NAME'] = "퀘스트 도우미 아이콘 자동 숨기기",
+    ['SETTINGS_AUTO_HIDE_QUEST_HELPER_ICONS_DESC'] = "이 기능을 사용하면 지원되는 퀘스트 도우미 애드온에서 아이콘이 자동으로 숨겨집니다.\n\n|c00FF9696지원되는 애드온: %s",
+
+    ['SETTINGS_SORTING_DISABLED_OPTION'] = "정렬 안함",
+    ['SETTINGS_SORTING_BY_LEVEL_OPTION'] = "레벨",
+    ['SETTINGS_SORTING_BY_LEVEL_REVERSED_OPTION'] = "레벨(반전)",
+    ['SETTINGS_SORTING_BY_PERCENT_COMPLETED_OPTION'] = "완료율",
+    ['SETTINGS_SORTING_BY_RECENTLY_UPDATED_OPTION'] = "최근 승인",
+    ['SETTINGS_SORTING_BY_QUEST_PROXIMITY_OPTION'] = "퀘스트 거리",
+
+    -- Settings > Filters & Sorting > Filters
+
+    ['SETTINGS_FILTERS_HEADER'] = "개발자 옵션",
+
+    ['SETTINGS_DISABLE_FILTERS_NAME'] = "필터 사용안함 / 퀘스트 수동 추적",
+    ['SETTINGS_DISABLE_FILTERS_DESC'] = "자동 필더링 기능을 비활성화하여, 퀘스트를 수동으로 추적해야 합니다..\n\n|c00FF9696이 옵션을 선택 취소하면 필터링이 지워집니다.|r",
+
+    ['SETTINGS_CURRENT_ZONE_ONLY_NAME'] = "현재 지역만",
+    ['SETTINGS_CURRENT_ZONE_ONLY_DESC'] = "현재 지역 / 하위 지역과 관련된 퀘스트를 표시합니다.",
+
+    ['SETTINGS_QUEST_LIMIT_NAME'] = "퀘스트 제한",
+    ['SETTINGS_QUEST_LIMIT_DESC'] = "화면에 주어진 시간이 표시되는 퀘스트의 수를 제한합니다.",
+
+    ['SETTINGS_HIDE_COMPLETED_QUESTS_NAME'] = "완료된 퀘스트 숨김",
+    ['SETTINGS_HIDE_COMPLETED_QUESTS_DESC'] = "완료된 퀘스트를 표시합니다.",
+
+    ['SETTINGS_RESET_TRACKING_OVERRIDES_NAME'] = "추적 재설정",
+    ['SETTINGS_RESET_TRACKING_OVERRIDES_DESC'] = "모든 수동 퀘스트의 추적을 재설정합니다.",
+
+    -- Settings > Visual Settings
+
+    ['SETTINGS_VISUAL_TAB'] = "시각적 설정",
+
+    ['SETTINGS_FONT_SIZE_NAME'] = "글꼴 크기",
+    ['SETTINGS_FONT_COLOR_NAME'] = "글꼴 색상",
+
+    ['SETTINGS_BACKGROUND_ALWAYS_VISIBLE_NAME'] = "배경 항상 보임",
+    ['SETTINGS_BACKGROUND_ALWAYS_VISIBLE_DESC'] = "추적기의 배경이 항상 표시됩니다.",
+
+    ['SETTINGS_BACKGROUND_COLOR_NAME'] = "배경 색상",
+    ['SETTINGS_BACKGROUND_COLOR_DESC'] = "추적기의 배경 색상",
+
+    ['SETTINGS_QUEST_PADDING_NAME'] = "퀘스트 간격",
+
+    -- Settings > Visual Settings > Tracker Header Settings
+
+    ['SETTINGS_TRACKER_HEADER_ENABLED_DESC'] = "추적기 머리말의 표시 여부",
+
+    ['SETTINGS_TRACKER_HEADER_FORMAT_DESC'] = "추적기 머리말은 어떤 형식으로 합니까?",
+    ['SETTINGS_TRACKER_HEADER_FORMAT_QUESTS_OPTION'] = "퀘스트",
+    ['SETTINGS_TRACKER_HEADER_FORMAT_QUESTS_NUMBER_VISIBLE_OPTION'] = "퀘스트 (10/17)",
+    ['SETTINGS_TRACKER_HEADER_FORMAT_QUESTS_NUMBER_VISIBLE_TOTAL_OPTION'] = "퀘스트 (10/20)",
+
+    -- Settings > Visual Settings > Zone Header Settings
+
+    ['SETTINGS_ZONE_HEADER_ENABLED_DESC'] = "퀘스트 위에 접을 수 있는 지역 명칭을 추가합니다.",
+
+    -- Settings > Visual Settings > Quest Header Settings
+
+    ['SETTINGS_COLOR_HEADERS_BY_DIFFICULTY_NAME'] = "난이도 별 색상",
+    ['SETTINGS_COLOR_HEADERS_BY_DIFFICULTY_DESC'] = "레벨 난이도에 따라 퀘스트 제목 색상을 설정합니다.",
+
+    ['SETTINGS_QUEST_HEADER_FORMAT_DESC'] = "퀘스트 제목 형식을 세밀하게 제어합니다.\n\n|c00FF9696{{title}}|r\n퀘스트 제목\n\n|c00FF9696{{level}}|r\n퀘스트 레벨\n\n|c00FF9696{{zone}}|r\n퀘스트가 속한 지역",
+    ['SETTINGS_QUEST_HEADER_FORMAT_CLASSIC_OPTION'] = "클래식",
+    ['SETTINGS_QUEST_HEADER_FORMAT_LEVELS_OPTION'] = "레벨",
+
+    -- Settings > Visual Settings > Objective Settings
+
+    -- Settings > Frame Settings
+
+    ['SETTINGS_FRAME_TAB'] = "프레임 설정",
+
+    ['SETTINGS_LOCK_FRAME_NAME'] = "프레임 잠금",
+    ['SETTINGS_LOCK_FRAME_DESC'] = "프레임이 움직이지 않도록 합니다.",
+
+    ['SETTINGS_POSITIONX_NAME'] = "좌표 X",
+    ['SETTINGS_POSITIONY_NAME'] = "좌표 Y",
+
+    ['SETTINGS_WIDTH_NAME'] = "너비",
+    ['SETTINGS_MAX_HEIGHT_NAME'] = "최대 높이",
+
+    ['SETTINGS_RESET_POSITION_NAME'] = "위치 재설정",
+
+    ['SETTINGS_RESET_SIZE_NAME'] = "크기 재설정",
+
+    -- Settings > Advanced Settings
+
+    ['SETTINGS_ADVANCED_TAB'] = "추가 기능",
+
+    ['SETTINGS_DEVELOPER_HEADER'] = "개발자 옵션",
+    ['SETTINGS_DEVELOPER_MODE_NAME'] = "개발자 모드",
+    ['SETTINGS_DEVELOPER_MODE_DESC'] = "로그 및 기타 시각적 도우미를 활성화 합니다.",
+
+    ['SETTINGS_DEBUG_LEVEL_NAME'] = "디버그 레벨",
+
+    ['SETTINGS_LOCALE_HEADER'] = "현지화 설정",
+    ['SETTINGS_LOCALE_NAME'] = "UI 지역 선택",
+
+    ['SETTINGS_RESET_HEADER'] = "Butter Quest Tracker 재설정",
+    ['SETTINGS_RESET_TEXT'] = "이 버튼을 누르면 Butter Quest Tracker의 모든 구성 설정이 기본값으로 초기화 됩니다.",
+
+    ['SETTINGS_RESET_NAME'] = "설정 초기화",
+    ['SETTINGS_RESET_DESC'] = "Butter Quest Tracker의 모든 설정을 기본값으로 재설정 합니다.",
+    ['SETTINGS_ADVERT_TEXT'] = "|c00FF9696Butter Quest Tracker는 월드 오브 워크래프트:클래식을 위해 활발히 개발 중 입니다. 알파 버전 또는 문제 보고를 위해 GitHub를 확인하십시오. \n\nhttps://github.com/butter-cookie-kitkat/ButterQuestTracker"
+};
